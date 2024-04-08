d8ff53e4882628be24976ef0dae0c77404685f36
Oleg Chetkin <oleg_448@mail.ru>
2024-04-08 16:10:30 +0300

    initial

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+
+int main()
+{
+    std::vector<int> lsdfj;
+    int i = 11;
+    int h = 110;
+    i = i * i * i * i * h;
+    float w;
+    w = 1.f;
+    std::cout << "Hello 123!\n" << i << w;
+    
+}
